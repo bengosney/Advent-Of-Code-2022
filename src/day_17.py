f30ad963f272f1d0ed3bab4d83f68bb752ca01ab
Ben Gosney <bengosney@googlemail.com>
2022-12-17 18:09:26 +0000

    Swap to indexes for loop detection

--- src/day_17.py
+++ src/day_17.py
@@ -1,7 +1,5 @@
 # Standard Library
-from collections.abc import Iterable
 from dataclasses import dataclass
-from itertools import cycle
 from typing import Self
 
 # First Party
@@ -55,16 +53,18 @@ class Game:
         self.board: dict[complex, str] = {}
         for x in range(7):
             self.board[complex(x, 0)] = "="
-        self.jets: Iterable[complex] = cycle(jets)
-        self.shapes: Iterable[Shape] = cycle(shapes)
+        self.jets: list[complex] = jets
+        self.shapes: list[Shape] = shapes
         self.rock: Shape | None = None
-        self.rock_count: int = 0
-        self.jet_count: int = 0
+        self.rock_idx: int = 0
+        self.jet_idx: int = 0
         self.spawn: int = SPAWN_GAP
         self.last_jet: str = ""
 
         self.draw_step = 0
         self.pause: bool = False
+
+        self.cache = {}
 
     @property
     def height(self):
@@ -72,8 +72,8 @@ class Game:
 
     def init_rock(self) -> bool:
         if self.rock is None:
-            self.rock = next(self.shapes) + complex(2, self.spawn)
-            self.rock_count += 1
+            self.rock = self.shapes[self.rock_idx % len(self.shapes)] + complex(2, self.spawn)
+            self.rock_idx += 1
             return True
         return False
 
@@ -81,8 +81,8 @@ class Game:
         if self.rock is None:
             raise Exception("No rock to move down")
 
-        m = next(self.jets)
-        self.jet_count += 1
+        m = self.jets[self.jet_idx % len(self.jets)]
+        self.jet_idx += 1
         rock = self.rock + m
 
         self.last_jet = "left" if m == LEFT else "right"
@@ -100,13 +100,13 @@ class Game:
         rock = self.rock + DOWN
         if any(r in self.board for r in rock.points):
             for r in self.rock.points:
-                self.board[r] = str(self.rock_count % len(SHAPES))
+                self.board[r] = str(self.rock_idx % len(SHAPES))
             self.spawn = max(self.spawn, self.rock.top + SPAWN_GAP)
             self.rock = None
         else:
             self.rock += DOWN
 
-    def round(self, input: str = ""):
+    def round(self):
         self.init_rock()
         self.move()
         self.down()
@@ -116,7 +116,7 @@ def part_1(input_string: str) -> int:
     jets = [LEFT if d == "<" else RIGHT for d in input_string]
     game = Game(jets, SHAPES)
 
-    while game.rock_count < 2023:
+    while game.rock_idx < 2023:
         game.round()
 
     return game.height
@@ -126,13 +126,8 @@ def part_2(input_string: str) -> int:
     jets = [LEFT if d == "<" else RIGHT for d in input_string]
     game = Game(jets, SHAPES)
 
-    rocks = 1000000000000
-    chunk = 10000
-
-    while game.rock_count < rocks:
+    while game.rock_idx < 1000000000000:
         game.round()
-        if (game.rock_count % chunk) == 0:
-            print(rocks - game.rock_count)
 
     return game.height
 
@@ -150,8 +145,8 @@ def test_part_1():
 
 
 # def test_part_2():
-#    test_input = get_example_input()
-#    assert part_2(test_input) is not None
+#   test_input = get_example_input()
+#   assert part_2(test_input) == 1514285714288
 
 
 # @no_input_skip

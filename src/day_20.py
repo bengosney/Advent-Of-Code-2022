45eedf990236c0b265824fe9454f107e666d4982
Ben Gosney <bengosney@googlemail.com>
2022-12-21 15:48:34 +0000

    Part 2

--- src/day_20.py
+++ src/day_20.py
@@ -6,15 +6,17 @@ from collections.abc import Iterable
 from utils import no_input_skip, read_input
 
 
-def mix(numbers: deque[tuple[int, int]]) -> list[int]:
-    for n in list(numbers):
-        v, _ = n
-        numbers.rotate(-numbers.index(n, 0))
-        numbers.popleft()
-        numbers.rotate(-v)
-        numbers.appendleft(n)
+def mix(numbers: deque[tuple[int, int]], rounds: int = 1) -> deque[tuple[int, int]]:
+    ordered = list(numbers)
+    for _ in range(rounds):
+        for n in ordered:
+            v, _ = n
+            numbers.rotate(-numbers.index(n, 0))
+            numbers.popleft()
+            numbers.rotate(-v)
+            numbers.appendleft(n)
 
-    return [n[0] for n in numbers]
+    return numbers
 
 
 def grove_numbers(numbers: list[int]) -> list[int]:
@@ -26,22 +28,27 @@ def grove_numbers(numbers: list[int]) -> list[int]:
     ]
 
 
-def intput_to_pairs(input: str) -> Iterable[tuple[int, int]]:
+def intput_to_pairs(input: str, mul: int = 1) -> Iterable[tuple[int, int]]:
     for i, n in enumerate(map(int, input.split("\n"))):
-        yield (n, n * i)
+        yield (n * mul, n * i)
 
 
 def part_1(input: str) -> int:
     numbers = deque(intput_to_pairs(input))
 
-    numbers = mix(numbers)
+    numbers = [n[0] for n in list(mix(numbers))]
     ans = grove_numbers(numbers)
 
     return sum(ans)
 
 
 def part_2(input: str) -> int:
-    pass
+    numbers = deque(intput_to_pairs(input, 811589153))
+
+    numbers = [n[0] for n in list(mix(numbers, 10))]
+    ans = grove_numbers(numbers)
+
+    return sum(ans)
 
 
 # -- Tests
@@ -62,30 +69,9 @@ def test_part_1():
     assert part_1(test_input) == 3
 
 
-def test_grove():
+def test_part_2():
     test_input = get_example_input()
-    numbers = deque(intput_to_pairs(test_input))
-    mixed = mix(numbers)
-    grove = grove_numbers(mixed)
-    answer = [4, -3, 2]
-
-    assert grove == answer
-
-
-@no_input_skip
-def test_real_grove():
-    test_input = read_input(__file__)
-    numbers = deque(intput_to_pairs(test_input))
-    mixed = mix(numbers)
-    grove = grove_numbers(mixed)
-    answer = [1608, -2852, 8469]
-
-    assert grove == answer
-
-
-# def test_part_2():
-#     test_input = get_example_input()
-#     assert part_2(test_input) is not None
+    assert part_2(test_input) == 1623178306
 
 
 @no_input_skip
@@ -94,10 +80,10 @@ def test_part_1_real():
     assert part_1(real_input) == 7225
 
 
-# @no_input_skip
-# def test_part_2_real():
-#     real_input = read_input(__file__)
-#     assert part_2(real_input) == 548634267428
+@no_input_skip
+def test_part_2_real():
+    real_input = read_input(__file__)
+    assert part_2(real_input) == 548634267428
 
 
 # -- Main

6686a3e17b9d8676a0dfea6fc796cca4a414f0e9
Ben Gosney <bengosney@googlemail.com>
2022-12-22 13:54:40 +0000

    Clean up part 1 while I think about part 2

--- src/day_22.py
+++ src/day_22.py
@@ -1,10 +1,12 @@
 # Standard Library
+import re
 from collections import deque
+from collections.abc import Iterable
 from dataclasses import dataclass
-from typing import Self
+from typing import Literal, Self
 
 # First Party
-from utils import no_input_skip, read_input  # noqa
+from utils import no_input_skip, read_input
 
 
 @dataclass(frozen=True)
@@ -15,6 +17,8 @@ class Vec:
     def __add__(self, delta: Self) -> Self:
         return Vec(self.x + delta.x, self.y + delta.y)
 
+
+Move = Literal["R"] | Literal["L"]
 
 D_UP = Vec(0, -1)
 D_RIGHT = Vec(1, 0)
@@ -47,19 +51,17 @@ def draw(board, max_x, max_y):
     print()
 
 
+def get_moves(unparsed_moves: str) -> Iterable[Move | int]:
+    for move, turn in re.findall(r"(\d+)([RL]?)", unparsed_moves):
+        yield int(move)
+        if turn != "":
+            yield turn
+
+
 def part_1(input: str) -> int:
     unparsed_board, unparsed_moves = input.split("\n\n")
 
-    moves = []
-    num = ""
-    for m in unparsed_moves:
-        if m.isnumeric():
-            num += m
-        else:
-            moves.extend((int(num), m))
-            num = ""
-    if num != "":
-        moves.append(int(num))
+    moves = get_moves(unparsed_moves)
 
     position = None
     facing = deque([D_RIGHT, D_DOWN, D_LEFT, D_UP])
@@ -74,36 +76,39 @@ def part_1(input: str) -> int:
             max_x = max(max_x, x)
             if i in [".", "#"]:
                 board[Vec(x, y)] = i
-                if i == "." and position is None:
-                    position = Vec(x, y)
+            if i == "." and position is None:
+                position = Vec(x, y)
 
     if position is None:
         raise ValueError("No position found")
 
-    for m, move in enumerate(moves):
-        if move == "L":
-            facing.rotate(1)
-        elif move == "R":
-            facing.rotate(-1)
-        else:
-            for _ in range(move):
-                next_position = position + facing[0]
-                if next_position not in board:
-                    if facing[0] == D_UP:
-                        next_position = Vec(next_position.x, max_y)
-                    if facing[0] == D_DOWN:
-                        next_position = Vec(next_position.x, 0)
-                    if facing[0] == D_LEFT:
-                        next_position = Vec(max_y, next_position.y)
-                    if facing[0] == D_RIGHT:
-                        next_position = Vec(0, next_position.y)
-                    while next_position not in board:
-                        next_position = next_position + facing[0]
+    for move in moves:
+        match move:
+            case "L":
+                facing.rotate(1)
+            case "R":
+                facing.rotate(-1)
+            case int():
+                for _ in range(move):
+                    next_position = position + facing[0]
+                    if next_position not in board:
+                        if facing[0] == D_UP:
+                            next_position = Vec(next_position.x, max_y)
+                        if facing[0] == D_DOWN:
+                            next_position = Vec(next_position.x, 0)
+                        if facing[0] == D_LEFT:
+                            next_position = Vec(max_y, next_position.y)
+                        if facing[0] == D_RIGHT:
+                            next_position = Vec(0, next_position.y)
+                        while next_position not in board:
+                            next_position = next_position + facing[0]
 
-                if board[next_position] == "#":
-                    break
-                board[position] = ARROWS[facing[0]]
-                position = next_position
+                    if board[next_position] == "#":
+                        break
+                    board[position] = ARROWS[facing[0]]
+                    position = next_position
+            case _:
+                raise ValueError(f"Unrecognised move: {move}")
 
     return (1000 * (position.y + 1)) + (4 * (position.x + 1)) + SCORES[facing[0]]
 
@@ -142,10 +147,10 @@ def test_part_1():
 #     assert part_2(test_input) is not None
 
 
-# @no_input_skip
-# def test_part_1_real():
-#     real_input = read_input(__file__)
-#     assert part_1(real_input) is not None
+@no_input_skip
+def test_part_1_real():
+    real_input = read_input(__file__)
+    assert part_1(real_input) == 27436
 
 
 # @no_input_skip
@@ -158,6 +163,6 @@ def test_part_1():
 
 if __name__ == "__main__":
     real_input = read_input(__file__)
-
+    real_input = get_example_input()
     print(f"Part1: {part_1(real_input)}")
     print(f"Part2: {part_2(real_input)}")
